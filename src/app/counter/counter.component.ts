836a6831cecc8a316006d25d38d034666c67cb08
Daniel Goins <dwgoins98@gmail.com>
2025-02-17 15:10:09 -0500

    Added doc blocks for the counter component and the messages service

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -9,37 +9,55 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterComponent implements OnInit {
+  /**
+   * NgZone instance to run code outside of Angular's zone to avoid triggering change detection.
+   * @private
+   */
   private zoneOptOut = inject<NgZone>(NgZone);
+
+  /**
+   * The current count value.
+   */
   count = signal<number>(0);
 
+  /**
+   * A debug output message for the component.
+   * Logs a message to the console whenever it is re-evaluated.
+   * 
+   * @returns A string containing the debug output message.
+   */
   get debugOutput() {
     console.log('[Counter] "debugOutput" binding re-evaluated.');
     return 'Counter Component Debug Output';
   }
 
+  /**
+   * Decreases the count by 1.
+   */
   onDecrease() {
     this.count.update((previousCount) => previousCount - 1);
-    // console.log(this.count());
   }
 
+  /**
+   * Increases the count by 1.
+   */
   onIncrease() {
     this.count.update((previousCount) => previousCount + 1);
-    // console.log(this.count());
   }
 
+  /**
+   * Initializes the component.
+   * Sets the initial count to 0 after 4 seconds and logs a message after 5 seconds without triggering Angular's change detection.
+   */
   ngOnInit(): void {
     setTimeout(() => {
       this.count.set(0);
     }, 4000);
 
-    // Avoiding Zone Pollution
     this.zoneOptOut.runOutsideAngular(() => {
       setTimeout(() => {
         console.log('Timer expired');
       }, 5000);
     });
-
-    // Change detection is ran after both browser timeouts
-    // But with NgZone, the timer expire will not be checked by Zone.js
   }
 }
--- src/app/message/messages.service.ts
+++ src/app/message/messages.service.ts
@@ -1,3 +1,7 @@
+/**
+ * @fileoverview This file contains the MessagesService class which is responsible for managing messages.
+ */
+
 import { Injectable, signal } from '@angular/core';
 
 @Injectable({
@@ -5,8 +9,13 @@ import { Injectable, signal } from '@angular/core';
 })
 export class MessagesService {
   private messages = signal<string[]>([]);
+  // Read all messages as a readonly signal.
   allMessages = this.messages.asReadonly();
 
+  /**
+   * Adds a new message to the list of messages.
+   * @param message The message to be added.
+   */
   addMessage(message: string) {
     this.messages.update((oldMessages) => [...oldMessages, message]);
   }
