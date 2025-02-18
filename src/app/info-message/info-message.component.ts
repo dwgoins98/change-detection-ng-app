bdba77c0e843f5143f9ee504aee196d5c512d2ac
Daniel Goins <dwgoins98@gmail.com>
2025-02-18 10:35:25 -0500

    Added a dynamically populated info message, since that was the only one not changing.

--- src/app/info-message/info-message.component.ts
+++ src/app/info-message/info-message.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-info-message',
@@ -8,12 +8,19 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InfoMessageComponent {
+  isClicked: boolean = false;
+  clickCount = signal<number>(0);
+
+  clickMessage = signal<string>('');
+
   get debugOutput() {
     console.log('[InfoMessages] "debugOutput" binding re-evaluated.');
     return 'InfoMessage Component Debug Output';
   }
 
-  onLog() {
-    console.log("Clicked!")
+  onClick() {
+    this.isClicked = true;
+    this.clickCount.update((oldClickCount) => oldClickCount + 1);
+    this.clickMessage.set(`Clicked ${this.clickCount()} times`);
   }
 }
